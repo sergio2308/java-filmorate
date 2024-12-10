e34d06581bdf5d040138235789bc1438d91593d6
Sergey Petrov <serg2308n@yandex.ru>
2024-12-11 00:17:36 +0300

    fix: добавить обработку исключений

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,13 +1,23 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
 import java.time.LocalDate;
 
 @Data
 public class Film {
     private int id;
+
+    @NotBlank(message = "Название фильма не должно быть пустым")
     private String name;
+
+    @NotBlank(message = "Описание фильма не должно быть пустым")
     private String description;
+
+    @Past(message = "Дата выпуска должна быть в прошлом")
     private LocalDate releaseDate;
-    private int duration;
+
+    private int duration; // продолжительность в минутах
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,13 +1,24 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
 import java.time.LocalDate;
 
 @Data
 public class User {
     private int id;
+
+    @Email(message = "Email должен быть корректным")
     private String email;
+
+    @NotBlank(message = "Логин не должен быть пустым")
     private String login;
+
     private String name;
+
+    @Past(message = "Дата рождения должна быть в прошлом")
     private LocalDate birthday;
 }
