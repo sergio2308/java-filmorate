95d124d2c76c8e807ab97995cefb3fe300327317
Sergey Petrov <serg2308n@yandex.ru>
2024-12-15 21:06:27 +0300

    style: поправить стиль

--- src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 
 public class NotFoundException extends RuntimeException {
     private final HttpStatus status;
+    
     public NotFoundException(HttpStatus status, String message) {
         super(message);
         this.status = status;
