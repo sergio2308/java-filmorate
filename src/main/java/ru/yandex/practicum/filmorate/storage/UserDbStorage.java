68e404beacf29f572fda47793279407ca1f12f6b
Sergey Petrov <serg2308n@yandex.ru>
2025-07-02 12:22:56 +0300

    Вернуть класс UserDbStorage

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserDbStorage.java
@@ -0,0 +1,72 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.sql.*;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@Primary
+@Qualifier("dbStorage")
+@RequiredArgsConstructor
+public class UserDbStorage implements UserStorage {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public User createUser(User user) {
+        String sql = "INSERT INTO users (email, login, name, birthday) VALUES (?, ?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, user.getEmail());
+            stmt.setString(2, user.getLogin());
+            stmt.setString(3, user.getName());
+            stmt.setDate(4, Date.valueOf(user.getBirthday()));
+            return stmt;
+        }, keyHolder);
+
+        user.setId(keyHolder.getKey().longValue());
+        return user;
+    }
+
+    @Override
+    public User updateUser(User user) {
+        String sql = "UPDATE users SET email = ?, login = ?, name = ?, birthday = ? WHERE user_id = ?";
+        int updated = jdbcTemplate.update(sql, user.getEmail(), user.getLogin(), user.getName(), Date.valueOf(user.getBirthday()), user.getId());
+
+        if (updated == 0) {
+            throw new NotFoundException("User with id " + user.getId() + " not found");
+        }
+        return user;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        String sql = "SELECT * FROM users";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> mapRowToUser(rs));
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        String sql = "SELECT * FROM Users WHERE user_id = ?";
+        try {
+            return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> mapRowToUser(rs), id);
+        } catch (Exception e) {
+            throw new NotFoundException("User with id " + id + " not found");
+        }
+    }
+
+    private User mapRowToUser(ResultSet rs) throws SQLException {
+        return User.builder().id(rs.getLong("user_id")).email(rs.getString("email")).login(rs.getString("login")).name(rs.getString("name")).birthday(rs.getDate("birthday").toLocalDate()).build();
+    }
+}
